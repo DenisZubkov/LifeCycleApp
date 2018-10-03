fe700d30814fe97e512ab0b7837ad46a419a625d
Denis Zubkov <denzu@macbook-pro-denis.local>
2018-10-03 21:02:42 +0300

    Final commit

--- LifeCycleApp/ViewController.swift
+++ LifeCycleApp/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logTextView: UITextView!
+    var labelCreated = false
+    var eventLog = UserDefaults.standard.value(forKey: "EventLog") as? String ?? ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        logTextView.isEditable = false
+        logTextView.isSelectable = false
+        logTextView.text = ""
+        labelCreated = true
     }
 
-
+    func updateLog(method: String) {
+        eventLog = "\(eventLog) \(method)\n"
+        UserDefaults.standard.set(eventLog, forKey: "EventLog")
+        if labelCreated {
+            logTextView.text = eventLog
+            let range = NSMakeRange(logTextView.text.count - 1, 1);
+            logTextView.scrollRangeToVisible(range)
+        }
+    }
 }
 
